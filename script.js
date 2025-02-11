3db7213532496686af0f19356030ee9bcdea9e10
Madaminjon Esanov <samisalih024@gmail.com>
2025-02-11 12:39:56 +0500

    yangilandi

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const weddingDate = new Date('August 29, 2024 19:00:00').getTime();
+const weddingDate = new Date('February 19, 2025 18:00:00').getTime();
 
 function updateCountdown() {
     const now = new Date().getTime();
